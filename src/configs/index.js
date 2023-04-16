2d7dd90796c2a5be6511886bf970bac0c3ca7999
tieunt0906 <tieunt.dev97@gmail.com>
2023-04-16 08:41:47 +0700

    add docker

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,10 +1,30 @@
 const { A_WEEK } = require('../constants');
 const { formatNumber } = require('../utils/number');
 
+const {
+  PORT = 3000,
+
+  PEPPER,
+
+  MONGO_HOST,
+  MONGO_PORT,
+  MONGO_DATABASE,
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+
+  JWT_SECRET_KEY,
+  JWT_EXPIRES_TIME,
+} = process.env;
+
+const MONGO_URI =
+  MONGO_USERNAME && MONGO_PASSWORD
+    ? `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}?authSource=admin`
+    : `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  MONGO_URI: process.env.MONGO_URI,
-  PEPPER: process.env.PEPPER,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  JWT_EXPIRES_TIME: formatNumber(process.env.JWT_EXPIRES_TIME, A_WEEK),
+  PORT: PORT || 3000,
+  MONGO_URI,
+  PEPPER,
+  JWT_SECRET_KEY,
+  JWT_EXPIRES_TIME: formatNumber(JWT_EXPIRES_TIME, A_WEEK),
 };
